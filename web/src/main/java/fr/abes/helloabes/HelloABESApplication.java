bfb66115e394e7a9f0ac730aa811c64100ddb484
Duy Son Tran <tran@abes.fr>
2020-12-07 17:37:11 +0100

    Ajouter slash api dans url

--- web/src/main/java/fr/abes/helloabes/HelloABESApplication.java
+++ web/src/main/java/fr/abes/helloabes/HelloABESApplication.java
@@ -12,6 +12,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
@@ -48,6 +49,11 @@ public class HelloABESApplication extends SpringBootServletInitializer implement
 	 */
 	public static void main(String[] args) {
 		SpringApplication.run(HelloABESApplication.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(HelloABESApplication.class);
 	}
 
 	/**
